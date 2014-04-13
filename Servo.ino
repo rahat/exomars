ff33c9529d715aa90dad09c3cd4e1f78c91d61ab
Rahat Mahmud <rahatmahmud@students.ptechnyc.org>
2014-04-12 20:49:56 -0400

    Proof of Concept
    Move servo based on color. The Mindstorm NXT should sense the color.
    The user will be able to send the name of a color via text/email.

--- Servo.ino
+++ Servo.ino
@@ -6,23 +6,35 @@ Servo myservo;  // create servo object to control a servo
                 // a maximum of eight servo objects can be created 
  
 int pos = 0;    // variable to store the servo position 
+String color = "black";
  
 void setup() 
 { 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-} 
- 
- 
-void loop() 
-{ 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  if (color == "black") {
+  for(pos = 0; pos < 135; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
+  }
+  if (color == "white") {
+  for(pos = 0; pos < 45; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);       // waits 15ms for the servo to reach the position 
+  } 
+  }
+    if (color == "grey") {
+  for(pos = 0; pos < 90; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
+  }
 } 
+
+void loop() {
+
+
+}
